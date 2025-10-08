bbb9f51fbb965bb900e242747663aeb9880de945
yongss <bbl737898@gmail.com>
2025-10-08 10:21:08 +0900

    feat: @Timed로 시간 재기

---
+++ actuator/src/main/java/hello/order/v4/OrderConfigV4.java
@@ -0,0 +1,23 @@
+package hello.order.v4;
+
+import hello.order.OrderService;
+import hello.order.v3.OrderServiceV3;
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OrderConfigV4 {
+
+    @Bean
+    public OrderService orderService() {
+        return new OrderServiceV4();
+    }
+
+    @Bean
+    public TimedAspect timedAspect(MeterRegistry registry) {
+        return new TimedAspect(registry);
+    }
+}
